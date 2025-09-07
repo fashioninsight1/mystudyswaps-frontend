af240eb727926490f3806cd497649138c4efd4ab
fashioninsight1 <fashioninsight1@gmail.com>
2025-09-07 20:25:43 +0000

    Completely remove toaster references from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Switch, Route } from "wouter";
 import { queryClient, setAuthTokenGetter } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-// import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider, useAuth } from "@/contexts/AuthContext";
 import { useRoleUpdate } from "@/hooks/useRoleUpdate";
